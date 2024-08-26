77c28552cddde68664162867f60c6d72bb3bb91e
Yaksh Darji <95200752+dyaksh@users.noreply.github.com>
2024-08-26 11:10:53 +0530

    data

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -22,48 +22,7 @@ const brandsData: Brand[] = [
     image: "/images/brands/bhel.png",
     imageLight: "/images/brands/bhel.png",
   },
-  {
-    id: 3,
-    name: "Lineicons",
-    href: "https://lineicons.com",
-    image: "/images/brands/lineicons.svg",
-    imageLight: "/images/brands/lineicons-light.svg",
-  },
-  {
-    id: 4,
-    name: "Tailadmin",
-    href: "https://tailadmin.com",
-    image: "/images/brands/tailadmin.svg",
-    imageLight: "/images/brands/tailadmin-light.svg",
-  },
-  {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
-  },
-    {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
-  },
-  {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
-  },
-  {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
-  },
+  
 ];
 
 export default brandsData;
