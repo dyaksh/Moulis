b1d74962634f00d121237d5599a8d748014dc9c3
Yaksh Darji <95200752+dyaksh@users.noreply.github.com>
2024-08-26 11:07:01 +0530

    image

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,26 +1,5 @@
-import { Brand } from "@/types/brand";
 import Image from "next/image";
-import brandsData from "./brandsData";
-
-const Brands = () => {
-  return (
-    <section className="pt-16">
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
-            <div className="flex flex-wrap items-center justify-center rounded-sm bg-gray-light px-8 py-8 dark:bg-gray-dark sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]">
-              {brandsData.map((brand) => (
-                <SingleBrand key={brand.id} brand={brand} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default Brands;
+import { Brand } from "@/types/brand";
 
 const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { href, image, imageLight, name } = brand;
@@ -31,12 +10,27 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+        className="relative w-1/2 h-auto opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
       >
-        <Image src={imageLight} alt={name} fill className="hidden dark:block" />
-        <Image src={image} alt={name} fill className="block dark:hidden" />
+        <div className="relative h-0 pb-[50%]">
+          <Image
+            src={imageLight}
+            alt={name}
+            layout="fill"
+            className="hidden dark:block object-contain"
+            priority
+          />
+          <Image
+            src={image}
+            alt={name}
+            layout="fill"
+            className="block dark:hidden object-contain"
+            priority
+          />
+        </div>
       </a>
     </div>
   );
 };
 
+export default SingleBrand;
